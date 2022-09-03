68e8a1e6c105dec558621bbfc58d733054be260d
luka57lee <lucas57.lee@gmail.com>
2022-09-03 13:40:26 -0400

    fix dateTimeToDateString

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,5 @@
 export const dateTimeToDateString = (date) => {
   const year = date.getFullYear();
-  const month = date.getMonth + 1;
+  const month = date.getMonth() + 1;
   return `${year}-${month < 10 ? `0${month}` : month}-01`;
 };
